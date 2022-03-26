c361b56cc46049c11b21926ab71cae047c763504
xdongming <97573465+xdongming@users.noreply.github.com>
2022-03-26 10:10:30 +0800

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -33,3 +33,32 @@ plt.figure()
 plt.plot(x1, data, c='k')
 plt.plot(x2, [data[data_num - 1], *pred], c='b')
 plt.show()
+
+
+from time import time
+import datetime
+dtrain=xgb.DMatrix(Xtrain,Ytrain)
+param1 = {'objective':'reg:squarederror',"subsample":1,"max_depth":6,"eta":0.1,"gamma":0,"lambda":1,"alpha":0,"colsample_bytree":1,"colsample_bylevel":1,"colsample_bynode":1}
+num_round = 50
+time0 = time()
+cvresult1 = xgb.cv(param1, dtrain, num_round,nfold=5)
+print(datetime.datetime.fromtimestamp(time()-time0).strftime("%M:%S:%f"))
+fig,ax = plt.subplots(1,figsize=(15,8))
+#ax.set_ylim(top=5)
+ax.grid()
+ax.plot(range(1,num_round+1),cvresult1.iloc[:,0],c="red",label="train,original")
+ax.plot(range(1,num_round+1),cvresult1.iloc[:,2],c="orange",label="test,original")
+param2 = {'objective':'reg:squarederror',"eta":0.1}
+param3 = {'objective':'reg:squarederror',"eta":0.1}
+time0 = time()
+cvresult2 = xgb.cv(param2, dtrain, num_round,nfold=5)
+print(datetime.datetime.fromtimestamp(time()-time0).strftime("%M:%S:%f"))
+time0 = time()
+cvresult3 = xgb.cv(param3, dtrain, num_round,nfold=5)
+print(datetime.datetime.fromtimestamp(time()-time0).strftime("%M:%S:%f"))
+ax.plot(range(1,num_round+1),cvresult2.iloc[:,0],c="green",label="train,last")
+ax.plot(range(1,num_round+1),cvresult2.iloc[:,2],c="blue",label="test,last")
+ax.plot(range(1,num_round+1),cvresult3.iloc[:,0],c="gray",label="train,this")
+ax.plot(range(1,num_round+1),cvresult3.iloc[:,2],c="pink",label="test,this")
+ax.legend(fontsize="xx-large")
+plt.show()
